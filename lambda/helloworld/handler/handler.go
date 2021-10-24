acad2bb28955e9fe30a8b23190f985399adbe66b
Joseph Green <joseph.peter.green@gmail.com>
2021-10-24 11:50:29 +0100

    simple hello world response from gateway with an arg

--- lambda/helloworld/handler/handler.go
+++ lambda/helloworld/handler/handler.go
@@ -6,8 +6,22 @@ import (
 )
 
 type Event struct {
+	Name string `json:"name"`
 }
 
-func Handle(ctx context.Context, event Event) (string, error) {
-	return fmt.Sprintf("Hello World!"), nil
+type Response struct {
+	StatusCode int               `json:"statusCode"`
+	Headers    map[string]string `json:"headers"`
+	Body       string            `json:"body"`
+}
+
+func Handle(ctx context.Context, event Event) (Response, error) {
+	responseBody := fmt.Sprintf("Hello %s!", event.Name)
+	response := Response{
+		StatusCode: 200,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       responseBody,
+	}
+
+	return response, nil
 }
